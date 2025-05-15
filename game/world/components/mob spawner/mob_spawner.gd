eba706fe6da0f5e8aec9a9dcf4ae92e6b6f2b521
IacovlevMaxim <iacovlev.maxim@gmail.com>
2025-05-15 13:25:49 +0300

    Fixed ghost sprite and pathfinding

--- game/world/components/mob spawner/mob_spawner.gd
+++ game/world/components/mob spawner/mob_spawner.gd
@@ -10,37 +10,37 @@ func start():
 
 
 func _on_timer_timeout():
-    if not enabled: return
-    
-    var tile: Vector2i
+	if not enabled: return
+	
+	var tile: Vector2i
 
-    
-    var player_pos = Global.game.player.get_tile_pos()
-    
-    var spawn_range = 30  
-    
-    tile = Vector2i(
-        player_pos.x + randi_range(-spawn_range, spawn_range),
-        player_pos.y + randi_range(-spawn_range, spawn_range)
-    )
-    
-    var mob_def: MobDefinition = DataManager.mobs.pick_random()
-    var spawner: MobSpawnerDefinition = mob_def.spawner
-    
-    if spawner and spawner.can_spawn(tile, world):
-        var other_mob: BaseMob = get_world().get_closest_mob(tile)
-        if other_mob:
-            var dist: float = other_mob.distance_to_tile(tile)
-            if dist < spawner.min_distance_to_other:
-                return
-            elif other_mob.type == mob_def and dist < spawner.min_distance_to_same:
-                return
-            
-            other_mob = get_world().get_closest_mob(tile, mob_def)
-            if dist < spawner.min_distance_to_same:
-                return
-        
-        world.spawn_mob(mob_def, tile)
+	
+	var player_pos = Global.game.player.get_tile_pos()
+	
+	var spawn_range = 30  
+	
+	tile = Vector2i(
+		player_pos.x + randi_range(-spawn_range, spawn_range),
+		player_pos.y + randi_range(-spawn_range, spawn_range)
+	)
+	
+	var mob_def: MobDefinition = DataManager.mobs.pick_random()
+	var spawner: MobSpawnerDefinition = mob_def.spawner
+	
+	if spawner and spawner.can_spawn(tile, world):
+		var other_mob: BaseMob = get_world().get_closest_mob(tile)
+		if other_mob:
+			var dist: float = other_mob.distance_to_tile(tile)
+			if dist < spawner.min_distance_to_other:
+				return
+			elif other_mob.type == mob_def and dist < spawner.min_distance_to_same:
+				return
+			
+			other_mob = get_world().get_closest_mob(tile, mob_def)
+			if dist < spawner.min_distance_to_same:
+				return
+		
+		world.spawn_mob(mob_def, tile)
 
 
 func get_world()-> World:
